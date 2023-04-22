db5a72bcbdd75af32b9c94553b0179c84ba40506
Valentine <kerubovalentine1@gmail.com>
2023-04-22 15:59:07 +0300

    modified components

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+function Links({github, linkedin}) {
+  return (
+    <Fragment>
+        <h3>Links </h3>
+        <a href={github}>{github}</a>
+        <a href={linkedin}>{linkedin}</a>
+    </Fragment>
+  );
+}
+export default Links
